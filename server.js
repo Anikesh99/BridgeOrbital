b8101c0b03553a3f53d0686dc9cae58a4af9b9a2
Ebolaeater <53635104+Ebolaeater@users.noreply.github.com>
2020-07-01 20:25:31 +0800

    Please work

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 const index = require('./routes/index')
 
 app.use(index)
